5ec475e11c0835bb5a423122341c8d6cac1a0fc5
hzrl04 <hazrilirfan@gmail.com>
2026-02-06 01:08:19 +0800

    Update driver orders servlet

--- src/main/java/sapujerrapp/DriverOrdersServlet.java
+++ src/main/java/sapujerrapp/DriverOrdersServlet.java
@@ -57,6 +57,7 @@ public class DriverOrdersServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
 	}
 
 }
